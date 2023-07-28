e57472610601507bf765d6f04e4d0fffdc8c280b
Sergey <140590783+SergeyFrontEnd1@users.noreply.github.com>
2023-07-28 10:40:50 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var a = 35;
+var b = 'Привет, ';
+var db = 'мир!';
+var c = false;
+var d = 5;
+
+a = 456;
+
+a = prompt ('Напишите значение переменой а');
+
+if (a == 37) {
+  alert ('a = 37!');
+} else {
+  alert ('что-то другое')
+}
+
+for (var i =0; i < 5; i++) {
+alert (i);
+}
